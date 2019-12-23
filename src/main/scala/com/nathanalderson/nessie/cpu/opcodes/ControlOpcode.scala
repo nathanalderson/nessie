3aa4444fb597af828799e89b6b0f6c70a12104a3
Nathan Alderson <nathan.alderson@gmail.com>
2019-12-23 10:51:41 -0600

    Refactor control opcode matching

--- src/main/scala/com/nathanalderson/nessie/cpu/opcodes/ControlOpcode.scala
+++ src/main/scala/com/nathanalderson/nessie/cpu/opcodes/ControlOpcode.scala
@@ -7,24 +7,47 @@ import com.nathanalderson.nessie.{Bus, Data, Tick}
 
 object ControlOpcode {
   def apply(byte: Data, bus: Bus): (Opcode, Bus) = {
-    if (byte == 0x00.toByte) (BRK(), bus)
-    else if (byte == 0xd8.toByte) (CLD(), bus)
-    else if (byte == 0xe8.toByte) (INX(), bus)
-    else (NOP(), bus)
+    val singleByteOp = byte match {
+      case BRK.opcode => Some(BRK())
+      case CLD.opcode => Some(CLD())
+      case INX.opcode => Some(INX())
+      case _ => None
+    }
+    val op = singleByteOp.getOrElse((byte & 0xe0) >> 5 match {
+//      case 0x0 =>
+//      case 0x1 =>
+//      case 0x2 =>
+//      case 0x3 =>
+//      case 0x4 =>
+//      case 0x5 =>
+//      case 0x6 =>
+//      case 0x7 =>
+      case _ => NOP()
+    })
+    (op, bus)
   }
 }
 
+object BRK {
+  val opcode: Data = 0x00.toByte
+}
 case class BRK() extends Opcode {
   override def execute(registers: Registers, bus:  Bus): (Registers, Bus, Tick) =
     throw Halt()
 }
 
+object CLD {
+  val opcode: Data = 0xd8.toByte
+}
 case class CLD() extends Opcode {
   override def execute(registers: Registers, bus:  Bus): (Registers, Bus, Tick) = {
     (registers.copy(s=registers.s.clear(StatusRegister.mask_decimal)), bus, 2L)
   }
 }
 
+object INX {
+  val opcode: Data = 0xe8.toByte
+}
 case class INX() extends Opcode {
   override def execute(registers: Registers, bus:  Bus): (Registers, Bus, Tick) = {
     val newX: Register = (registers.x + 1.toByte).toByte
